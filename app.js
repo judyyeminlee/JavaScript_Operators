2ca10d397f8756cc46f0b9303376050e2228c15b
Judy Lee <judyyeminlee@gmail.com>
2024-06-06 16:10:51 -0400

    finishedExercise

--- app.js
+++ app.js
@@ -4,16 +4,78 @@ console.log(
 );
 
 // Exercise 1
-console.log("EXERCISE 1:\n==========\n");
+let a = 20
+let b = 4
+let add = a + b;
+let minus = a - b;
+let multiply = a * b;
+let dividing = a / b;
+console.log(add);
 
 // YOUR CODE HERE
 
 // Exercise 2
-console.log("EXERCISE 2:\n==========\n");
+let num = 11;
+let str = "11";
+let str2 = "eleven";
+let isPresent = true;
+let firstName = "Frodo";
+let lastName = "Baggins";
+
+num + str
+console.log(num + str); // 1111
+
+num + str2
+console.log(num + str2); // 11eleven
+
+num + isPresent
+console.log(num + isPresent); // 12
+
+firstName + num
+console.log(firstName + num); // Frodo11
+
+isPresent + str
+console.log(isPresent + str); // true11
+
+firstName + lastName
+console.log(firstName + lastName); // FrodoBaggins
 
 // YOUR CODE HERE
 
 // Exercise 3
+let val = 5;
+let str3 = "5";
+let str4 = "five";
+let isPresent2 = false;
+
+val == str3
+console.log(val ==str3); // true
+
+val === str3
+console.log(val ===str3); // false
+
+!isPresent2
+console.log(!isPresent2); // true
+
+("eleven" == str4 && val >= str3)
+console.log("eleven" == str4 && val >= str3); // false
+
+(!isPresent2 || isPresent2)
+console.log(!isPresent2 || isPresent2); // true
+
+0 == false
+console.log(0 == false); // true
+
+0 === false
+console.log(0 === false); // false
+
+0 != false
+console.log(0 != false); // false
+
+0 !== false
+console.log(0 !== false); // true
+
+
 console.log("EXERCISE 3:\n==========\n");
 
 // YOUR CODE HERE
